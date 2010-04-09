97d36e47bd15bc6e4986589d0d496357cd302d95
Andrew <andrew@ecttest.(none)>
2010-04-09 08:50:49 -0700

    Moving Cluck to Koalatrack Server

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,9 @@
-set :application, "cluck"
+set :application, "koalatrack"
 set :scm, :git
-set :repository,  "git@github.com:quasor/cluck.git"
+set :repository,  "git@github.com:quasor/koalatrack.git"
 set :branch, "master"
 set :deploy_via, :remote_cache
-set :user, 'user'
+set :user, 'andrew'
 set :ssh_options, { :forward_agent => true }
 
 
@@ -19,18 +19,21 @@ set :deploy_to, "/var/www/#{application}"
 
 set :use_sudo, false
 
-role :app, 'cluck.gotdns.com'
-role :web, 'cluck.gotdns.com'
-role :db,  'cluck.gotdns.com', :primary => true
+role :app, '172.30.145.57'
+role :web, '172.30.145.57'
+role :db,  '172.30.145.57', :primary => true
 
 namespace :deploy do
   desc "Restarting mod_rails with restart.txt"
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "touch #{current_path}/tmp/restart.txt"
   end
- 
+
   [:start, :stop].each do |t|
     desc "#{t} task is a no-op with mod_rails"
     task t, :roles => :app do ; end
   end
+
+
 end
+
